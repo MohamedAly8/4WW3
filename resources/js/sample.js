87bd7075160498340bc58f46696d17471ea5899f
Mohamed Aly <mohamedaly@Mohameds-MacBook-Pro.local>
2021-11-06 22:30:30 -0400

    Finished individual maps api

--- resources/js/sample.js
+++ resources/js/sample.js
@@ -3,7 +3,7 @@ var myLatlng = new google.maps.LatLng(43.6360091,-79.6269842);
 
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
+      zoom: 16, 
       center: { lat: 43.58134101996192, lng: -79.71344805588085 },
     });
 
@@ -15,4 +15,3 @@ function initMap() {
       title: "Burger Factory",
     });
   }
-  
