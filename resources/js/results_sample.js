a0238a5a254cdbdd1a07023c92b4528c71a58a91
Mohamed Aly <mohamedaly@Mohameds-MacBook-Pro.local>
2021-11-07 22:48:01 -0500

    Commented JS

--- resources/js/results_sample.js
+++ resources/js/results_sample.js
@@ -1,22 +1,25 @@
+// Init function initializes the map and needed parameters for the map 
 function init() {
-    //var lattitude = 59.325;
-    //var longtitude = 18.069;
-    const initialPosition = { lat: 26.846694, lng: 80.946166 };
+    // Initialized the center of the map to Downtown toronto
+    const initialPosition = { lat: 43.5592074, lng: -79.3326367 };
+    // Sets the map instance to the div with ID map from the html to place map in that location in the html
     const map = new google.maps.Map(document.getElementById('map'), {
+        // Centers the map in the beginning the the coords specified in initialPosition
         center: initialPosition,
+        // The initial zoom of the map
         zoom: 15
     });
-    //const marker = new google.maps.Marker({ map, position: initialPosition });
+    // Initialized a variable for the pop ups for each marker to display info about restaurants
      const infoWindow = new google.maps.InfoWindow();
 
     // Get user's location
-
+    // Gets the user's current position using the geolocation API
     if ('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition(
             position => {
                 console.log(`Lat: ${position.coords.latitude} Lng: ${position.coords.longitude}`);
       
-
+                // Initialize variables for images to be used as icons for the User position on the map (image1) and the restaurants (imgage2)
                 const image =
                     "https://developers.google.com/maps/documentation/javascript/examples/full/images/library_maps.png"
                 const image2 = 
@@ -24,7 +27,7 @@ function init() {
 
                 
 
-
+                // A list of attributes for each marker on the map. Each marker has a Latitude and Longtitude, A message, A label and an icon image
                 const stops = [
                     [{lat: position.coords.latitude, lng: position.coords.longitude}, "<h1>You Are Here:</h1> <br></br> Latitude: " + position.coords.latitude + "<br></br>Longitude: "+ position.coords.longitude + "<br></br>", "", image],
                     [{lat: position.coords.latitude - 0.005, lng: position.coords.longitude + 0.005}, "Ozzy's", "A", image2],
@@ -36,8 +39,10 @@ function init() {
 
                   
                 
-                  // Create the markers.
+                  // Create the markers using a for loop for each index of "stops" list above.
                   stops.forEach(([position, title, label, icon], i) => {
+                      // Each marker has a position, title, label. Optimized is set to be false to make the labels static. 
+                      // Also an additional href is added to each title to link each restaurant with its individual restaurant review page
                     const marker = new google.maps.Marker({
                       position,
                       map,
@@ -48,6 +53,8 @@ function init() {
                     });
                 
                     // Add a click listener for each marker, and set up the info window.
+                    // The Listener opens the infoWindow onclick and retrieves the market Title and sets the content in the info Window to
+                    // Title. The marker and its Title info is shown on the map till the user clicks else where
                     marker.addListener("click", () => {
                       infoWindow.close();
                       infoWindow.setContent(marker.getTitle());
@@ -61,11 +68,12 @@ function init() {
                     lng: position.coords.longitude
                 });
             },
+            // Alerts the user for when there is an error with finding the position of the user 
             err => alert(`Error (${err.code}): ${getPositionErrorMessage(err.code)}`)
         );
 
        
-
+            // If the Geolocation is not  supported by browser, an error is alerted to the user.
     } else {
         alert('Geolocation is not supported by your browser.');
     }
--- resources/js/sample.js
+++ resources/js/sample.js
@@ -1,6 +1,9 @@
+// A variable that holds a new object from the google maps API being a set of Lat and Long from a specific restuarant location
 var myLatlng = new google.maps.LatLng(43.6360091,-79.6269842);
 
-
+// This function initializes a map instance, where it is placed in the div wit the id "map" in the results_sample.html page
+// The zoom parameter specifes how zoomed in the initialized map is
+// The center parameter is where the map will be centered when it is first rendered on the page
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 16, 
@@ -8,7 +11,9 @@ function initMap() {
     });
 
     
-
+// This marker variable is a visible red marker that will be shown on the map instantiated above 
+// The position parameter is the location of the market, map parameter indicates which map this marker will be placed
+// The title is shown when the user hovers on the market on the initialized map
     const marker = new google.maps.Marker({
       position: { lat: 43.58134101996192, lng: -79.71344805588085 },
       map,
