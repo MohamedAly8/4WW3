2ddeeb69417bda9a8a94d2e70f56bd5ebb4b3537
khanm294 <mehakrafi2000@gmail.com>
2021-11-07 22:48:04 -0500

    added code comments for signup js and html

--- resources/js/signup.js
+++ resources/js/signup.js
@@ -1,10 +1,12 @@
 function verifyEmail() {
+  // Get the email value from the form
     var x = document.myform.email.value
-
+// This is to validate that email type is correct ie. contains @, does not start with a '.', etc.
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(x)) {
       document.getElementById("messsageEmail").innerHTML = "";
       return (true);
     }
+    //If email type is incorrect display error message
     else {
       document.getElementById("messsageEmail").innerHTML = "You have entered an invalid email address!";
       return (false)
@@ -22,28 +24,31 @@ function verifyEmail() {
       return false;
     }
 
-    //minimum length validation 
+    //minimum length validation, display error message if less than 8 characters
     if (pw.length < 8) {
       document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";
       return false;
     }
+    //if less not less than 8 characters, no error message
     else {
       document.getElementById("message").innerHTML = "";
     }
 
-    //maximum length validation  
+    //maximum length validation, display error message if greater than 15 characters
     if (pw.length > 15) {
       document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";
       return false;
     }
+    //if not greater than 15 characters, no error message
     else {
       document.getElementById("message").innerHTML = "";
         }
-    //equality of passwords validation
+    //equality of passwords validation, if not equal then display error message
     if (pw != pw2) {
       document.getElementById("messageMismatch").innerHTML = "Passwords did not match";
       return false;
     } else {
+      //at this point, password is fully validated so return true with no error message
       document.getElementById("messageMismatch").innerHTML = "";
       return true;
     }
