7985e21fa82ed7e65fdfa54a3d91f4d5961285e7
Mohamed Aly <mohamedaly@Mohameds-MacBook-Pro.local>
2021-12-08 16:07:14 -0500

    Still fixing map

--- individual_sample.php
+++ individual_sample.php
@@ -19,15 +19,17 @@ include('createDatabase.php');
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
-    crossorigin="anonymous"></script>
-    <script src="/resources/js/sample.js"></script>
+
 
   <title>Sample Results</title>
 </head>
 
 <body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+    crossorigin="anonymous"></script>
+    <script src="/resources/js/sample.js"></script>
 
   <!-- Bootstrap navbar, added style to change color -->
   <nav class="navbar navbar-expand-lg navbar-light" style="background-color: lightgreen;">
