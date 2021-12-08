923120f62b66fb6348f50474796df00b74dd0dbd
Mohamed Aly <mohamedaly@Mohameds-MacBook-Pro.local>
2021-12-08 16:47:29 -0500

    Fixing up map issues

--- results_sample.php
+++ results_sample.php
@@ -74,7 +74,7 @@ include('createDatabase.php');
 
     <div id="result_map">
     <script>
-    var result_map = L.map('map').setView([43.58318179804465, -79.63780996170709], 13);
+    var result_map = L.map('result_map').setView([43.58318179804465, -79.63780996170709], 13);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
